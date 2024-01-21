ca69857e5984b8f8b2ed3acb631771c886222b3b
Mujahed Ali <mujahed97@gmail.com>
2024-01-21 16:43:31 -0500

    finished feed  post index

--- db/migrate/20240120174838_change_first_last_name_null_false.rb
+++ db/migrate/20240120174838_change_first_last_name_null_false.rb
@@ -1,7 +1,28 @@
-class ChangeFirstLastNameNullFalse < ActiveRecord::Migration[7.0]
-  def change
-    change_column :users, :first_name, :string, null: false
-    change_column :users, :last_name, :string, null: false
+# class ChangeFirstLastNameNullFalse < ActiveRecord::Migration[7.0]
+#   def change
+#     change_column :users, :first_name, :string, null: false
+#     change_column :users, :last_name, :string, null: false
 
+#   end
+# end
+class ChangeFirstLastNameNullFalse < ActiveRecord::Migration[7.0]
+  def up
+    # Update existing records with a default value or non-null value for "first_name"
+    User.where(first_name: nil).update_all(first_name: "DefaultFirstName")
+
+    # Update existing records with a default value or non-null value for "last_name"
+    User.where(last_name: nil).update_all(last_name: "DefaultLastName")
+
+    # Change column to not allow null values for "first_name"
+    change_column :users, :first_name, :string, null: false
+
+    # Change column to not allow null values for "last_name"
+    change_column :users, :last_name, :string, null: false
+  end
+
+  def down
+    # Revert changes if needed
+    change_column :users, :first_name, :string, null: true
+    change_column :users, :last_name, :string, null: true
   end
 end

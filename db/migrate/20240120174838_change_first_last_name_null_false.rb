950fff052870c50685da4510a76e1e4eb5b24c1d
Mujahed Ali <mujahed97@gmail.com>
2024-01-21 16:41:03 -0500

    finished feed  post index

--- db/migrate/20240120174838_change_first_last_name_null_false.rb
+++ db/migrate/20240120174838_change_first_last_name_null_false.rb
@@ -1,8 +1,5 @@
 class ChangeFirstLastNameNullFalse < ActiveRecord::Migration[7.0]
   def change
-
-    User.where(first_name: nil).update_all(first_name: "DefaultFirstName")
-    User.where(first_name: nil).update_all(last_name: "DefaultLastName")
     change_column :users, :first_name, :string, null: false
     change_column :users, :last_name, :string, null: false
 
